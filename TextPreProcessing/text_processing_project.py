0709d64ce11183506da3177f8902b7436a273cc0
AryaHisma <aryahisma@yahoo.co.id>
2024-08-23 23:48:26 +0700

    update app dan cache dan dashboard

--- TextPreProcessing/text_processing_project.py
+++ TextPreProcessing/text_processing_project.py
@@ -25,7 +25,7 @@ def download_nltk_resources():
         os.makedirs(nltk_data_path)
 
     # Mengunduh resource dan menentukan direktori target
-    nltk.download('punkt', download_dir=nltk_data_path)
+    nltk.download('punkt_tab', download_dir=nltk_data_path)
     nltk.download('stopwords', download_dir=nltk_data_path)
     
 # Panggil fungsi untuk mengunduh resource yang diperlukan
